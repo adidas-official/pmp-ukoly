8037b2bbb39cd81674b69cde56d30e8650f78386
zfrydryn <zdenek.frydryn@gmail.com>
2026-03-17 22:51:05 +0100

    Spinner + fix activity
    Vyber kodu bank. PoC prevodu dat z jedne aktivity do druhe.

--- ukol02/app/src/main/java/com/example/login/SendPaymentActivity.java
+++ ukol02/app/src/main/java/com/example/login/SendPaymentActivity.java
@@ -3,8 +3,10 @@ package com.example.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,7 @@ public class SendPaymentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_send_payment);
         cancelPayment();
         createPayment();
+        copyMessage();
     }
 
     private void cancelPayment() {
@@ -30,11 +33,31 @@ public class SendPaymentActivity extends AppCompatActivity {
         });
     }
 
+    private void copyMessage() {
+        Button copy_message_button = (Button) findViewById(R.id.button_copy_message);
+        EditText note = (EditText) findViewById(R.id.editText_note);
+        EditText note2rec = (EditText) findViewById(R.id.editText_note2rec);
+
+        copy_message_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                note2rec.setText(note.getText().toString());
+            }
+        });
+    }
+
+
     private void createPayment() {
         EditText bankaccount = (EditText) findViewById(R.id.editText_an);
-        EditText bankcode = (EditText) findViewById(R.id.editTex_bankcode);
+        Spinner bankcode = (Spinner) findViewById(R.id.spinner_bankcode);
+
+        ArrayAdapter<CharSequence> codes = ArrayAdapter.createFromResource(this, R.array.bankcodes, android.R.layout.simple_spinner_item);
+        codes.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        bankcode.setAdapter(codes);
+
         EditText amount = (EditText) findViewById(R.id.editText_amount);
         EditText ks = (EditText) findViewById(R.id.editText_ks);
+        EditText ss = (EditText) findViewById(R.id.editText_ss);
         EditText vs = (EditText) findViewById(R.id.editText_vs);
         EditText note = (EditText) findViewById(R.id.editText_note);
         EditText note2rec = (EditText) findViewById(R.id.editText_note2rec);
@@ -49,9 +72,11 @@ public class SendPaymentActivity extends AppCompatActivity {
                     double payment = Double.parseDouble(amount.getText().toString());
                     if (payment < balance) {
                         Intent checkPayment = new Intent("com.example.login.checkPaymentActivity");
+                        checkPayment.putExtra("payment", payment);
                         checkPayment.putExtra("bankaccount", bankaccount.getText().toString());
-                        checkPayment.putExtra("bankcode", bankcode.getText().toString());
+                        checkPayment.putExtra("bankcode", bankcode.getSelectedItem().toString());
                         checkPayment.putExtra("ks", ks.getText().toString());
+                        checkPayment.putExtra("ss", ss.getText().toString());
                         checkPayment.putExtra("vs", vs.getText().toString());
                         checkPayment.putExtra("note", note.getText().toString());
                         checkPayment.putExtra("note2rec", note2rec.getText().toString());
